a8eb7c29cfcac989c29efcf4ddcd6ccf2cb1dc25
dvinubius <dvinubiu.narmon@gmail.com>
2024-07-02 18:00:48 +0300

    more convenient event checks

--- packages/subscribable/example.lua
+++ packages/subscribable/example.lua
@@ -28,6 +28,7 @@ sub.load() -- LOAD SUBSCRIBABLE CAPABILITIES
   HANDLERS that may trigger the events
     - "increment" -> Counter is incremented
     - "setGreeting" -> Greeting is set
+    - "setGreetingAsGmVariant" -> Greeting is set as a randomly generated variant of "GM"
     - "updateAll" -> Counter and Greeting are updated
 ]]
 
@@ -35,13 +36,20 @@ Counter = Counter or 0
 
 Greeting = Greeting or "Hello"
 
+-- Define CUSTOM TOPICS and corresponding CHECK FUNCTION
+-- Check Functions use global state to determine if the event is occurring
+sub.configTopics({
+  ['even-counter'] = internal.checkNotifyCounter,
+  ['gm-greeting'] = internal.checkNotifyGreeting
+})
+
 Handlers.add(
   'increment',
   Handlers.utils.hasMatchingTag('Action', 'Increment'),
   function(msg)
     Counter = Counter + 1
-    -- notifications are sent by the subscribable PACKAGE, based on a conditions defined by THIS process
-    sub.checkNotifyTopic('even-counter', internal.checkNotifyCounter, msg.Timestamp)
+    -- Check and send notifications if event occurs
+    sub.checkNotifyTopic('even-counter', msg.Timestamp)
   end
 )
 
@@ -50,8 +58,18 @@ Handlers.add(
   Handlers.utils.hasMatchingTag('Action', 'SetGreeting'),
   function(msg)
     Greeting = msg.Tags.Greeting
-    -- notifications are sent by the subscribable PACKAGE, based on a conditions defined by THIS process
-    sub.checkNotifyTopic('gm-greeting', internal.checkNotifyGreeting, msg.Timestamp)
+    -- Check and send notifications if event occurs
+    sub.checkNotifyTopic('gm-greeting', msg.Timestamp)
+  end
+)
+
+Handlers.add(
+  'setGreetingAsGmVariant',
+  Handlers.utils.hasMatchingTag('Action', 'SetGreetingAsGmVariant'),
+  function(msg)
+    Greeting = 'GM-' .. tostring(math.random(1000, 9999))
+    -- We know for sure that notifications should be sent --> this helps to avoid performing redundant computation
+    sub.notifyTopic('gm-greeting', msg.Timestamp)
   end
 )
 
@@ -61,7 +79,7 @@ Handlers.add(
   function(msg)
     Greeting = msg.Tags.Greeting
     Counter = msg.Tags.Counter
-    -- notifications are sent by the subscribable PACKAGE, based on a conditions defined by THIS process
+    -- Check for multiple topics and send notifications if event occurs
     sub.checkNotifyTopics({
       ['even-counter'] = internal.checkNotifyCounter,
       ['gm-greeting'] = internal.checkNotifyGreeting
--- packages/subscribable/src/main.lua
+++ packages/subscribable/src/main.lua
@@ -5,6 +5,8 @@ local dispatcher = require "dispatcher"
 
 local AOCRED = 'Sa0iBLPNyJQrwpTTG-tWLQU-1QeUAJA73DdxGGiKoJc'
 
+local topicsCfg = {}
+
 mod.load = function()
   Handlers.add(
     "RegisterSubscriber",
@@ -22,9 +24,30 @@ mod.load = function()
   )
 end
 
-mod.checkNotifyTopics = function(topicsAndChecks, timestamp)
-  for topic, checkFn in pairs(topicsAndChecks) do
-    local notify, payload = checkFn(topic)
+mod.configTopics = function(cfg)
+  topicsCfg = cfg
+end
+
+-- dispatch without check
+
+mod.notifyTopics = function(topicsAndPayloads, timestamp)
+  for topic, payload in pairs(topicsAndPayloads) do
+    payload.timestamp = timestamp
+    dispatcher.dispatch(topic, payload)
+  end
+end
+
+mod.notifyTopic = function(topic, payload, timestamp)
+  return mod.notifyTopics({
+    [topic] = payload
+  }, timestamp)
+end
+
+-- dispatch with configured checks
+
+mod.checkNotifyTopics = function(topics, timestamp)
+  for _, topic in ipairs(topics) do
+    local notify, payload = topicsCfg[topic].checkFn()
     payload.timestamp = timestamp
     if notify then
       dispatcher.dispatch(topic, payload)
@@ -32,11 +55,8 @@ mod.checkNotifyTopics = function(topicsAndChecks, timestamp)
   end
 end
 
-mod.checkNotifyTopic = function(topic, checkFn, timestamp)
-  return mod.checkNotifyTopics({
-    topic = topic,
-    checkFn = checkFn
-  }, timestamp)
+mod.checkNotifyTopic = function(topic, timestamp)
+  return mod.checkNotifyTopics({ topic }, timestamp)
 end
 
 return mod
