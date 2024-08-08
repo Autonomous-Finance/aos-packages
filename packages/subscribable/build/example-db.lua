0e3a7422d33280a76aaa7f734b088d425916b2ba
dvinubius <dvinubiu.narmon@gmail.com>
2024-08-08 12:58:01 +0300

    version bump

--- packages/subscribable/build/example-db.lua
+++ packages/subscribable/build/example-db.lua
@@ -625,7 +625,7 @@ local function newmodule(cfg)
   local pkg = Subscribable or
       { useDB = cfg.useDB } -- useDB can only be set on initialization; afterwards it remains the same
 
-  pkg.version = '1.3.4'
+  pkg.version = '1.3.5'
 
   -- pkg acts like the package "global", bundling the state and API functions of the package
 
